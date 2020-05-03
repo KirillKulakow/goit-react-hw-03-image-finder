948de758cd43e7d8e2ef9039eb0cd777bd92e480
Kirill <kirillkulakow31@gmail.com>
2020-05-03 18:02:36 +0300

    Updates

---
+++ precache-manifest.f113cd97e0a95f77140a99368a2f9f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b1bcca53e5b59a83536974730fd784",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "2842e72d5c8fd0895162",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.f581c6bd.chunk.css"
+  },
+  {
+    "revision": "d6ee37cc4a65104ef660",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9c5fc598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9c5fc598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2842e72d5c8fd0895162",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.a52f58e1.chunk.js"
+  },
+  {
+    "revision": "ef7f04d3f12739069562",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.79cd0c33.js"
+  }
+]);

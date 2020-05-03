a2ebd7b3c5b9893e035b2be46143b71c4bb4a60f
Kirill <kirillkulakow31@gmail.com>
2020-05-03 20:35:13 +0300

    Updates

---
+++ precache-manifest.b599ccdb8d7cf94a8d1f356e8de99586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15eb327588f5b0a27e318adcbeee6ab0",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "691d54b8ae31bbaa4dab",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.cb792e0c.chunk.css"
+  },
+  {
+    "revision": "ed7b3fcf0b81c9f818c6",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.93930555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.93930555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691d54b8ae31bbaa4dab",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c1b7c7ee.chunk.js"
+  },
+  {
+    "revision": "ef7f04d3f12739069562",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.79cd0c33.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.557b97c1474c2ef4f00cb9f0df7ac937.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.b599ccdb8d7cf94a8d1f356e8de99586.js"
 );
 
 self.addEventListener('message', (event) => {
